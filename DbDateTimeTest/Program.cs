75c4106cd62446359b48bf817d2dfef729b50636
George Nychis <george@softwareroboticscorp.com>
2015-04-30 13:21:06 -0400

    initial checkin

---
+++ DbDateTimeTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbDateTimeTest
+{
+    [Serializable]
+    public class MyClasse
+    {
+        [Key]
+        public DateTime dateTime { get; set; }
+    }
+
+    public class MyContext : DbContext
+    {
+        public DbSet<MyClasse> MyClasses { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store it in the queue
+            MyClasse original = new MyClasse();
+            original.dateTime = DateTime.Now;
+
+            // Store it in the database
+            var db = new MyContext();
+            db.Database.ExecuteSqlCommand("TRUNCATE TABLE dbo.MyClasses");
+            db.MyClasses.Add(original);
+            db.SaveChanges();
+
+            // Check the database version
+            MyClasse dbItem = db.MyClasses.FirstOrDefault();
+            Console.WriteLine("DB Item:");
+            Console.WriteLine("  * Original: " + original.dateTime.ToString());
+            Console.WriteLine("  * DB Item: " + dbItem.dateTime.ToString());
+            Console.WriteLine("  * Equal? " + dbItem.dateTime.Equals(original.dateTime));
+
+            Console.ReadLine();
+        }
+    }
+}
